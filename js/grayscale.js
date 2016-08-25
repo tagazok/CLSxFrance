f2016e282a4e7e1ba822927081525bcbee76cbd4
Olivier Leplus <leplus.olivier@gmail.com>
2016-08-25 15:30:04 +0200

    fix(blink) : Menu button had mobile behaviour even on desktop

--- js/grayscale.js
+++ js/grayscale.js
@@ -6,7 +6,7 @@
 
 // jQuery to collapse the navbar on scroll
 function collapseNavbar() {
-    if ($(".navbar").offset().top > 50) {
+    if ($(".navbar").offset().top > 150) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
@@ -29,7 +29,9 @@ $(function() {
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-    $(this).closest('.collapse').collapse('toggle');
+    if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+        $('.navbar-toggle:visible').click();
+    }
 });
 
 // Google Maps Scripts
